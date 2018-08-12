bc07c1091490c9c6d6857f3b0f8d428ae90b5f5a
Kenny Nguyen <kenny@Marilyns-MacBook-Pro.local>
2018-08-11 22:59:56 -0400

    set up spa

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import '../styles/index.scss';
-import 'wired-elements';
 
+import HomePage from './visual/Home';
 
 class App extends Component {
+
+  switchPages() {
+    
+  }
   render() {
     return (
-      <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <wired-input placeholder="Enter name"></wired-input>
-        <wired-button >Submit</wired-button>
+      <div>
+        <HomePage />
       </div>
     );
   }
