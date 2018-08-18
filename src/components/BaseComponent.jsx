9f6d5e49aac1544a7f01a1c494c33d114f40ccf8
Kenny Nguyen <kenny@Marilyns-MacBook-Pro.local>
2018-08-18 17:37:52 -0400

    refactor Hoc Redux

--- src/components/BaseComponent.jsx
+++ src/components/BaseComponent.jsx
@@ -1,47 +1,67 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
-import application from '../redux/application/actionDispatchers';
+import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import PropTypes from 'prop-types'
+import allActions from '../redux/actionDispatchers';
 
-import '../styles/index.scss';
-import 'wired-elements';
-
-const mapState = (state) => {
-  return {
-    application: state.application,
-  }
-};
-
-const mapDispatch = (dispatch) => ({
-    updatePage: (newPage) => dispatch(application.updatePage(newPage)),
-});
-
-class BaseComponent extends Component {
+/**
+ * HoC that wraps Application actions + whatever actions you need.
+ * @param {ReactNode} ComposedComponent 
+ * @param {Object} connectObject
+ * @param {Object} connectObject.states - Array of states to map
+ * @param {Object} connectObject.actions - Array of actions to map
+ */
+export default function (ComposedComponent, connectObject) {
 
   /**
-   * Update page state
-   * @param {String} page 
+   * Iterate through passed states and map the state to Component to be returned
+   * @param {state} state 
    */
-  navigateTo(page) {
-    console.log('BaseComponent::', page)
-    this.props.updatePage(page);
-    // TODO
-  }
+  const mapStatetoProps = (state) => {
+    return (connectObject.states || []).reduce((acc, item) => {
+      acc[item] = state[item];
+      return acc;
+    },
+      {
+        //Default all components have to extend this
+        'application': state['application'],
+      }
+    );
+  };
 
   /**
-   * Start loading state
+   * Iterate through passed actions and map the action to Component to be returned
+   * @return Object with at LEAST the applicaiton actions
    */
-  startLoading() {
-    // TODO
-    // loading state = true
-  }
+  const actionCreators = () => {
+    return Object.assign(
+      (connectObject.actions || []).reduce((acc, item) => acc[item] = allActions[item], {}),
+      ...allActions.applicationActions
+    )
+  };
 
-  /**
-   * Stop loading state
-   */
-  stopLoading() {
-    //TODO
+  class ReduxContainer extends React.PureComponent {
+    constructor(props) {
+      super(props)
+      const { dispatch } = props
+      this.boundActionCreators = bindActionCreators(actionCreators, dispatch)
+    }
+    render() {
+      return (
+        <ComposedComponent
+          {...this.props}
+          {...this.boundActionCreators}
+        />
+      )
+    }
   }
+  
+  ReduxContainer.propTypes = {
+    dispatch: PropTypes.func
+  }
+  ReduxContainer.defaultProps = {
+    dispatch: () => { }
+  }
+  return connect(mapStatetoProps)(ReduxContainer)
 }
-
-export default connect(mapState, mapDispatch)(BaseComponent);
