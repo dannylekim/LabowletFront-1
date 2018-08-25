d77d4295031c3d02acd2e680424d7bf0676e3fed
Kenny Nguyen <alphakennyn@gmail.com>
2018-08-25 16:43:28 -0400

    add test for Hoc

--- src/test/components/connectToRedux.test.js
+++ src/test/components/connectToRedux.test.js
@@ -1,20 +1,46 @@
-
-// process.env.BABEL_ENV = 'test';
-// process.env.NODE_ENV = 'test';
-// process.env.PUBLIC_URL = '';
-
+import React from 'react'
 import { expect } from 'chai';
 import assert from 'assert';
 import sinon from 'sinon';
+import configureStore from 'redux-mock-store';
 
 // import React from 'react';
 import { mount, render, shallow} from 'enzyme';
+import connectToRedux from '../../../src/components/connectToRedux';
 
+const mockStore = configureStore();
+
+const myReducers = require('../../redux/rootReducer');
+
+require('../testSetup.js');
+
+const TempComponent = () => {
+  return (
+    <div>This is temp component</div>
+  );
+};
 
 describe('HOC::connectToRedux', () => {
-  describe('this works', () => {
+  describe('Structure', () => {
     it('should be a function', () => {
-      assert(true);
+      expect(connectToRedux).to.be.an('function');
     })
   })
+
+  describe('extension', () => {
+    let component;
+
+    beforeAll(() => {
+      const ConnectedComponent = connectToRedux(TempComponent, { states: [] , actions: [] });
+      console.log(myReducers);
+      const wrapper = shallow(<ConnectedComponent store={mockStore({})} />)
+      component = wrapper.dive();
+    })
+
+    it('should return object' , () => {
+      console.log(component.props().store.getState());
+      // const connectObject = connectToRedux(TempComponent, { states: [] , actions: [] });
+      expect(component.props().store.getState().application).to.be.an('object');
+    }) 
+  });
 });
