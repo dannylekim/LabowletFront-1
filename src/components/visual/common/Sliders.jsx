9977483654c8f32c7f5b4fd30f3e357fd295354e
Kenny Nguyen <kenny>
2018-08-31 18:58:50 -0400

    add slider as common component

---
+++ src/components/visual/common/Sliders.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
+const handleStyle = {
+  backgroundColor: '#000000',
+  border: '#000000',
+};
+
+const trackStyle = {
+  backgroundColor: '#000000',
+  border: '#000000',
+  transition: 'all 300ms cubic-bezier(0.42, -0.33, 0.5, 1.33) 0s',
+}
+
+const dotStyle = {
+  backgroundColor: '#000000',
+  border: '#000000',
+
+}
+
+
+const Sliders = (props) => {
+  return (
+    <Slider
+      min={2}
+      max={6}
+      defaultValue={3}
+      marks={1}
+      dots
+      onChange={(e) => props.handleChange(e)}
+      handleStyle={handleStyle}
+      trackStyle={trackStyle}
+      dotStyle={dotStyle}
+    />
+  );
+}
+
+export default Sliders;
