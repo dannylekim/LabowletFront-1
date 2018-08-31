f611e5ea9d742682a77ca5dcb5317ffd34f5efc5
Kenny Nguyen <kenny>
2018-08-31 18:58:37 -0400

    add create page

--- src/components/visual/CreatePage.jsx
+++ src/components/visual/CreatePage.jsx
@@ -1,53 +1,74 @@
-  import React, { PureComponent }  from 'react';
+import React, { PureComponent } from 'react';
 
-  import ReduxConnector from '../ReduxConnector';
-  import Slider from 'rc-slider';
-  import 'rc-slider/assets/index.css';
+import ReduxConnector from '../ReduxConnector';
 
-  import '../../styles/create.scss';
+import Slider from './common/Sliders';
+import Switch from 'rc-switch';
 
-  class CreatePage extends PureComponent {
-    constructor() {
-      super();
-      this.state = {
-        name: '',
-      }
-    }
+import 'rc-switch/assets/index.css';
 
-    _handleSubmit(e) {
-      console.log(e);
-    }
+import '../../styles/create.scss';
 
-    render() {
-      return (
-        <div className="page create">
-          <div className='navbar'>
-            <h2>Create Room</h2>
-          </div>
+const RoundType = [
+  { round: 1, name: 'Round 1' },
+  { round: 2, name: 'Round 2' },
+  { round: 3, name: 'Round 3' },
+]
 
-          <div className='page-container'>
-            <form onSubmit={(e) => this._handleSubmit(e)}>
-              <h3>Max number of words</h3>
-              <Slider 
-                min={2}
-                max={6}
-                defaultValue={3}
-                marks={1}
-              />
-              <h3>Time limit</h3><input type="time" name="time"/>
-              <h3>Allow skip:</h3><input type="checkbox" name="skippable"/>
-              <button 
-                className='generic-button'
-                type='submit'
-              >
-                Create
-              </button>
-            </form>
-          </div>
-        </div>
-      );
+class CreatePage extends PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
     }
   }
+
+  _handleSubmit(e) {
+    console.log(e);
+  }
+
+  handleNumberWords(value) {
+    console.log(value);
+  }
+
+  handleTime(value) {
+    console.log(value)
+  }
+
+  render() {
+    const renderRoundList = RoundType.map((value) => (<li><span>{value.name}</span> <input type="checkbox" name={`round${value.round}`} /></li>))
+    return (
+      <div className="page create">
+        <div className='navbar'>
+          <div className="back-button" onClick={() => this.props.updatePage('HOME')}>Back</div>
+          <h2>Create Room</h2>
+        </div>
+
+        <div className='page-container'>
+          <form onSubmit={(e) => this._handleSubmit(e)}>
+            <h3>Max number of words</h3>
+            <Slider handleChange={(value) => this.handleNumberWords(value)} />
+            <h3>Time limit</h3>
+            <Slider handleChange={(value) => this.handleNumberWords(value)} />
+            <hr />
+            <div className="create-select-rounds">
+              <span><h3>Allow skip:</h3><Switch className='can-skip-switch' /></span>
+              <ul>
+                {renderRoundList}
+              </ul>
+            </div>
+            <button
+              className='generic-button'
+              type='submit'
+            >
+              Create
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
 
 const connectObject = {
   states: [],
