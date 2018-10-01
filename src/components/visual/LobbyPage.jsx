6b573f325f66a989ec67b0c7ced1a8c2164b521a
Kenny Nguyen <alphakennyn@gmail.com>
2018-10-01 01:41:40 -0400

    add team cards in lobby room

--- src/components/visual/LobbyPage.jsx
+++ src/components/visual/LobbyPage.jsx
@@ -16,6 +16,7 @@ class LobbyPage extends PureComponent {
     super(props);
     this.state = {
     }
+    this._joinTeam = this._joinTeam.bind(this);
   }
 
   /**
@@ -25,14 +26,22 @@ class LobbyPage extends PureComponent {
   _renderTeam(maxTeams) {
     const TeamCardArray = [];
     for(let i = 0; i< maxTeams; i++) {
-      TeamCardArray.push(<TeamCard />)
+      TeamCardArray.push(<TeamCard index={i + 1} key={i+1} joinTeam={this._joinTeam} teamMates={'todo'}/>)
     }
+    
+    return TeamCardArray;
+  }
+
+  _joinTeam(teamId) {
+    console.log('joined team', teamId)
   }
 
   render() {
     const roomCode = this.props.room.code || 'UH OH';
     const settings = {...this.props.room.settings}
     const benchPlayers = [...settings.benchPlayers].map((player) => <PlayerIcon key={player.id} name={player.name}/>);
+    const teamList = this._renderTeam(10);//settings.maxTeams);
+
     return (
       <div className="page home">
         <div className='navbar'>
@@ -41,7 +50,7 @@ class LobbyPage extends PureComponent {
         <div className="page-container">
           <p>Code is</p>
           <h1>{roomCode}</h1>
-
+          {teamList}
         </div>
         <div className="page-footer">
           <div className="foot-header">
--- src/components/visual/common/TeamCard.jsx
+++ src/components/visual/common/TeamCard.jsx
@@ -1,16 +1,64 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
-const TeamCard = (props) => {
-  return (
-    <div className="team-card" onClick={(e) => props.joinTeam(e)}>
-      <div className="team-card-title">
-        <h3>TEAM {props.team}</h3>
+import '../../../styles/teamCards.scss';
+
+const ORIGINAL_STYLES = {
+  opacity: '0',
+  transform: 'scale(0.1)',
+}
+const MOUNTED_STYLES = {
+  opacity: '1',
+  transform: 'scale(1)',
+}
+const CARD_TRANSITION = 'cubic-bezier(0.18, 0.89, 0.32, 1.28)';
+
+class TeamCard extends PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      styles: Object.assign(ORIGINAL_STYLES),
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.mountStyles();
+    }, 40 + this.props.index * 10);
+  }
+
+  componentWillUnmount() {
+    setTimeout(() => {
+      this.unmountStyles();
+    }, 30);
+  }
+
+  mountStyles() {
+    this.setState((prev) => ({
+      styles: Object.assign({
+        transition: `600ms ${CARD_TRANSITION} ${100 * this.props.index}ms`
+      }, MOUNTED_STYLES),
+    }));
+  }
+
+  unmountStyles() {
+    this.setState((prev) => ({
+      styles: Object.assign(prev.styles, ORIGINAL_STYLES),
+    }));
+  }
+  
+  render() {
+    return (
+      <div className="team-card" onClick={() => this.props.joinTeam(this.props.index)} style={this.state.styles}>
+        <div className="team-card-title">
+          <h3>TEAM {this.props.index}</h3>
+        </div>
+        <div className="team-card-container">
+          {this.props.teamMates}
+        </div>
       </div>
-      <div className="team-card-container">
-        {props.teamMates}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default TeamCard;
