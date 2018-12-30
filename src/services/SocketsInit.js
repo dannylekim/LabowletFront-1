01c6fa590109cbf5d36efe7678a72ab5a4b401dd
Kenny Nguyen <kenny>
2018-12-30 11:41:46 -0500

    establish connection to scoket

--- src/services/SocketsInit.js
+++ src/services/SocketsInit.js
@@ -1,6 +1,7 @@
-import io from 'socket.io-client';
 import configs from '../config/RESTurl.json';
-
+import SockJS from 'sockjs-client';
+import STOMP from 'stompjs';
+//const multichannelClient = require('sockjs-rooms').client;
 const LABOWLET_PATH = '/labowlet/'
 
 /**
@@ -19,21 +20,41 @@ class LabowletSocket {
   }
   
   connect(address, path){
-    const socket = io(address, {
-      path,
+    const socket = new SockJS(`${address}${path}`);
+    console.log(socket)
+    const socketClient = STOMP.over(socket);
+    socketClient.connect({}, function (frame) {
+        console.log('Connected: ' + frame);
+        // Make factory method that creates subsriptions with `/room/${this.roomCode}` prefix thus the params is the socket event.
+        socketClient.subscribe(`/room/${this.roomCode}/join`, function (payload) {
+            console.log('fuck yea');
+            console.log(payload);
+        });
     });
+    // const socketClient = socket.channel(`/room/${this.roomCode}`, (data) => {
+    //   console.log(data)
+    // });
 
-    socket.on('connection', (mySocket) => {
-      mySocket.join(`/room/${this.roomCode}`);
-      console.log(socket.connected); // true
-    });
-        
-    //Add events here...
-    socket.on(``, (payload) => {
-      console.log('wtf is this', payload);
-    })
+    // const socket = io(address, {
+    //   path,
+    // });
+
+    // console.log(socket);
+
+    // socket.on('connect', (mySocket) => {
+    //   mySocket.join(`/room/${this.roomCode}`, (data) => {
+    //     console.log(data);
+    //   });
+    //   console.log('yay connected!'); // true
+    //   console.log(socket.connected); // true
+    // });
+
+    // //Add events here...
+    // socket.on(``, (payload) => {
+    //   console.log('wtf is this', payload);
+    // })
     
-    return socket;
+    return socketClient;
   }
 
   get socketConnection() {
