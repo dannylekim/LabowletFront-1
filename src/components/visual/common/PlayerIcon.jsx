2a2fe96705b2f8e738670ed091341f0bfe0bab26
Kenny Nguyen <alphakennyn@gmail.com>
2019-01-11 23:03:01 -0500

    add player ready state

--- src/components/visual/common/PlayerIcon.jsx
+++ src/components/visual/common/PlayerIcon.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 import '../../../styles/playericon.scss';
 
 const PlayerIcon = (props) => {
+  const color = props.color ? props.color : 'white';
   const firstLetter = props.name.toUpperCase().charAt(0)
   return (
-    <div className='player-icon'>
+    <div className='player-icon' style={{ backgroundColor: color}}>
       <p>{firstLetter}</p>
     </div>
   );
