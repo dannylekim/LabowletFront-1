6b4a86e9f8e7fa45626ff2d595fa33037114ac0d
Kenny Nguyen <kenny.nguyen@dynamicly.com>
2019-07-14 11:25:00 -0400

    improve game ui

--- src/components/visual/GamePage.jsx
+++ src/components/visual/GamePage.jsx
@@ -33,14 +33,16 @@ class Actor extends PureComponent {
         {
           this.state.ready ?
             (<div className="game-container__actor-area">
-              <h3 className="game-container__word">{this.props.word}</h3>
+              The word is
+              <h2 className="game-container__word">{this.props.word}</h2>
+              { (this.props.canSkip && this.props.maxWords === 1) && 'Last word! Can\'t skip anymore.'}
               <div className="game-container__actions">
-                {this.props.canSkip && <button onClick={() => this.props.handleSkip()}>Skip</button>}
+                {this.props.canSkip && <button disabled={this.props.maxWords === 1} onClick={() => this.props.handleSkip()}>Skip</button>}
                 <button onClick={() => this.props.handleGotIt()}>Got it!</button>
               </div>
             </div>) : (
               <div className="game-container__actions">
-                <button onClick={(e) => this.handleReady(e)}>Ready</button>
+                <button className="ready-state" onClick={(e) => this.handleReady(e)}>Ready</button>
               </div>
             )
         }
@@ -94,7 +96,8 @@ class GamePage extends PureComponent {
       case 'ACTOR':
         return (
           <Actor
-            // canSkip={this.props.room.roomSettings}
+            canSkip={this.props.room.settings.roomSettings.allowSkips}
+            maxWords={this.props.game.remainingWords}
             gameType={this.props.game.gameType}
             word={this.props.game.currentWord}
             handleGotIt={() => this.props.sendWord(this.props.game.currentWord)}
@@ -123,7 +126,7 @@ class GamePage extends PureComponent {
       <div className="game">
         <div className="page-container">
           <div className="game-type">
-            {gameType}
+            {gameType.replace('_', ' ')}
           </div>
           <div className="game-timer">
             <h2>
