064885122a7f79f5b5689eabdcb5c11a8778105a
Kenny Nguyen <alphakennyn@gmail.com>
2019-07-16 00:38:31 -0400

    add new socket calls

--- src/redux/game/actionDispatchers.js
+++ src/redux/game/actionDispatchers.js
@@ -1,4 +1,5 @@
 import * as action from './actions';
+import { updatePage } from '../application/actions'; 
 
 const updatePoints = (points) => {
   return (dispatch) => dispatch(action.updatePoints(points));
@@ -39,10 +40,44 @@ const startStep = () => {
   }
 }
 
+/**
+ * a socket call to reset the game. It will return
+  NULL game to the socket endpoint
+  NULL wordState to the socket endpoint
+  and the currentRoom to the room endpoint
+ */
+const resetGame = () => {
+  return async (dispatch, getState) => {
+    try {
+      const newSettings = await getState().user.socket.send(`/room/${getState().room.code}/game/resetGame`, {});
+      console.log(newSettings);
+    } catch (err) {
+      throw new Error(`Error while starting new turn/round: ${err.message}`);
+    }
+  }
+}
+
+const leaveRoom = () => {
+  return async (dispatch, getState) => {
+    try {
+      if (getState().user.socket) {
+        await getState().user.socket.send(`/server/room/${getState().room.code}/leaveRoom`, {});
+      }
+      dispatch(updatePage('HOME'));
+    } catch (err) {
+      throw new Error(`Error while starting new turn/round: ${err.message}`);
+    }
+  }
+}
+
 export default { 
   updatePoints,
   updateStatus,
   updateContent,
+
+  // game socket calls
   sendWord,
   startStep,
+  resetGame,
+  leaveRoom,
 };
