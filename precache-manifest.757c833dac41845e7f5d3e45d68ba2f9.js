6a2008aa122554bbe7ec67c36affbd9a3f156000
Kenny Nguyen <kenny.nguyen@dynamicly.com>
2019-09-01 15:02:00 -0400

    Updates

---
+++ precache-manifest.757c833dac41845e7f5d3e45d68ba2f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01aefdb49e2376ba2244378b9a09e100",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d800f93032c0fd97a0",
+    "url": "/static/css/2.78a77095.chunk.css"
+  },
+  {
+    "revision": "f4a38b6c84006f9e98bd",
+    "url": "/static/css/main.4cd9cd12.chunk.css"
+  },
+  {
+    "revision": "88d800f93032c0fd97a0",
+    "url": "/static/js/2.7085b791.chunk.js"
+  },
+  {
+    "revision": "f4a38b6c84006f9e98bd",
+    "url": "/static/js/main.d7ba79e8.chunk.js"
+  },
+  {
+    "revision": "dfc89e28b1eea2969690",
+    "url": "/static/js/runtime~main.49ab3b9d.js"
+  },
+  {
+    "revision": "387e7e3577602eadb9c60760301ad96b",
+    "url": "/static/media/leafy.387e7e35.woff"
+  }
+]);
