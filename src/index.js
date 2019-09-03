19a94b77855c8086703dd204175929b09e23b8d0
Kenny Nguyen <kenny.nguyen@dynamicly.com>
2019-09-03 09:24:55 -0400

    successfully migrate to crav3

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { Provider } from 'react-redux';
 
 import store from './redux/configureStore';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
+import  './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -12,5 +13,7 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
-
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
