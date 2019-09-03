d4e325f1129a5ece1e6c5f7fade6cdfe172e931f
Kenny Nguyen <kenny.nguyen@dynamicly.com>
2019-09-03 09:28:17 -0400

    Updates

---
+++ precache-manifest.add950114307ae1482022d0dbe0c7df9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cae3fc611040c61bb251cce47357006",
+    "url": "/LabowletFront/index.html"
+  },
+  {
+    "revision": "626db2ae7fefe070e2f3",
+    "url": "/LabowletFront/static/css/2.78a77095.chunk.css"
+  },
+  {
+    "revision": "a18bb064cf75187258c4",
+    "url": "/LabowletFront/static/css/main.8e564cb3.chunk.css"
+  },
+  {
+    "revision": "626db2ae7fefe070e2f3",
+    "url": "/LabowletFront/static/js/2.e535b4ef.chunk.js"
+  },
+  {
+    "revision": "a18bb064cf75187258c4",
+    "url": "/LabowletFront/static/js/main.b14e11fd.chunk.js"
+  },
+  {
+    "revision": "1d6d90fbf73a4516ae8c",
+    "url": "/LabowletFront/static/js/runtime~main.60ecec61.js"
+  },
+  {
+    "revision": "387e7e3577602eadb9c60760301ad96b",
+    "url": "/LabowletFront/static/media/leafy.387e7e35.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.757c833dac41845e7f5d3e45d68ba2f9.js"
+  "/LabowletFront/precache-manifest.add950114307ae1482022d0dbe0c7df9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/LabowletFront/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
